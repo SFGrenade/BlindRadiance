872bbffff1030758a108c2d0ab4c866736a78723
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2020-06-28 13:53:37 +0200

    Initial commit

---
+++ BlindRadiance/BlindRadiance.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using Modding;
+
+namespace BlindRadiance
+{
+    public class BlindRadiance : Mod
+    {
+        internal static BlindRadiance Instance;
+
+        private SceneChanger sceneChanger;
+
+        public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+            Instance = this;
+
+            sceneChanger = new SceneChanger
+            {
+                modBase = this
+            };
+            initCallbacks();
+
+            Log("Initialized");
+        }
+
+        private void initCallbacks()
+        {
+            // Hooks
+            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OnSceneChanged;
+        }
+
+        private void OnSceneChanged(UnityEngine.SceneManagement.Scene from, UnityEngine.SceneManagement.Scene to)
+        {
+            string scene = GameManager.instance.GetSceneNameString();
+            if ((scene == "Dream_Final_Boss") || (scene == "Dream_Final") || (scene == "GG_Radiance"))
+            {
+                // Path of Pain Entrance, needs change to make "Test of Teamwork" accessible
+                GameManager.instance.StartCoroutine(sceneChanger.Change_Dream_Final_Boss(to));
+            }
+        }
+    }
+}
---
+++ BlindRadiance/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BlindRadiance
+{
+    class SceneChanger : MonoBehaviour
+    {
+        public BlindRadiance modBase { get; set; }
+
+        public IEnumerator Change_Dream_Final_Boss(UnityEngine.SceneManagement.Scene scene)
+        {
+            modBase.Log("Change_Dream_Final_Boss()");
+            yield return null;
+
+            GameManager.instance.StartCoroutine(PatchBackground(scene));
+        }
+
+        private IEnumerator PatchBackground(UnityEngine.SceneManagement.Scene scene)
+        {
+            yield return null;
+            modBase.Log("Patching Background");
+
+            GameObject plane = GameObject.Find("BlurPlane");
+            GameObject backgroundDim = GameObject.Instantiate(plane, null, true);
+            backgroundDim.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Sprites/Default"));
+            backgroundDim.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0, 128);
+
+            modBase.Log("Background Patched");
+        }
+    }
+}
